576db123e4129246bc4096c03c09d47f13ee2278
Marcio Anderson <marcio.anderson.ti@gmail.com>
2022-11-29 09:47:05 -0300

    continuidade HEC

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,3 +1,12 @@
-const numero = prompt('Digite um número: ');
+const numero = Number(prompt('Digite um número: '));
 const numeroTitulo = document.getElementById('numero-titulo');
 const pegaTexto = document.getElementById('texto-pretexto');
+
+numeroTitulo.innerHTML = numero;
+pegarTexto.innerHTML = '';
+pegaTexto.innerHTML += `Raiz Quadrada${numero ** 0.5}</strong><br />`;
+pegaTexto.innerHTML += `É NaN: ${Number.isNaN(numero)}</strong><br />`;
+pegaTexto.innerHTML += `${numero} é inteiro: <strong>${Number.isInteger(numero)}</strong> <br />`;
+pegaTexto.innerHTML += `Arrendondando para baixo: <strong>${Math.floor(numero)}</strong><br />`;
+pegaTexto.innerHTML += `Arrendondando para cima: <strong>${Math.ceil(numero)}</strong><br />`;
+pegaTexto.innerHTML += `Com duas casas decimais: <strong>${numero.toFixed(2)}</strong><br />`;
